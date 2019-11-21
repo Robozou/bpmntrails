cfaa4aa0fbadd255d3c8703f594367734693a2d7
fujfuj <57488731+fujfuj@users.noreply.github.com>
2019-11-21 18:35:28 +0100

    Trying stuff to make the builder work.

--- DCRReader/XMLBuilder.cs
+++ DCRReader/XMLBuilder.cs
@@ -6,57 +6,107 @@ namespace DCRReader
 {
     class XMLBuilder
     {
-        
+        int padding = 0;
         // make the trail in here
         BPMNTrail trail = new BPMNTrail();
         private Processor graph;
         private List<List<EventNode>> trace;
-        private Dictionary<int, List<Tuple<string, int>>> tree;
+        private Dictionary<string, List<Tuple<string, string>>> tree;
 
         public XMLBuilder(Processor graph, List<List<EventNode>> trace)
         {
             this.graph = graph;
             this.trace = trace;
-            this.tree = new Dictionary<int, List<Tuple<string, int>>>();
+            this.tree = new Dictionary<string, List<Tuple<string, string>>>();
+        }
+
+        public void Print()
+        {
+            trail.PrintTrail();
         }
 
         public void Build()
         {
             GrowTree();
+            //Console.WriteLine(tree.Keys.Count);
+            //Console.ReadLine();
             graph.Load();
             trail.AddStartEvent(graph.GetHashCode()+"");
-            List<Tuple<string, int>> tuples = tree[graph.GetHashCode()];
-            foreach(Tuple<string,int> tuple in tuples)
+            AddTasks();
+            AddFlows();
+        }
+
+        private void AddFlows()
+        {
+            string current;
+            foreach (string key in tree.Keys)
             {
-                
+                if (tree[key].Count > 1)
+                {
+                    current = "exgate" + padding;
+                    trail.AddExclusiveGateway(current, false);
+                    trail.AddSequenceFlow("seqflow" + padding, key + "", current);
+                    padding++;
+                } else
+                {
+                    current = key + "";
+                }
+                if (tree[key].Count > 0) {
+                    foreach (Tuple<string, string> transition in tree[key])
+                    {
+                        trail.AddSequenceFlow("seqflow" + padding, current, transition.Item2 + "");
+                        padding++;
+                    }
+                } else
+                {
+                    current = "end" + padding;
+                    trail.AddEndEvent(current);
+                    trail.AddSequenceFlow("seqflow" + padding, key+"", current);
+                    padding++;
+                }
+            }
+        }
+
+        private void AddTasks()
+        {
+            foreach(string key in tree.Keys)
+            {
+                foreach(Tuple<string, string> transition in tree[key])
+                {
+                    if (!trail.ContainsEvent(transition.Item2 + ""))
+                    {
+                        trail.AddTask(transition.Item2+"", transition.Item1);
+                    }
+                }
             }
         }
 
         private void GrowTree()
         {
-            int state;
+            string state;
             string id;
-            int newState;
-            List<Tuple<string, int>> tuples;
+            string newState;
+            List<Tuple<string, string>> tuples;
             foreach (List<EventNode> ls in trace)
             {
                 foreach (EventNode n in ls)
                 {
                     state = graph.GetHashCode();
                     id = n.id;
-                    graph.execute(id);
+                    graph.Execute(id);
                     newState = graph.GetHashCode();
+                    Console.WriteLine(newState);
                     if (tree.ContainsKey(state))
                     {
                         tuples = tree[state];
-                        tuples.Add(new Tuple<string, int>(id, newState));
+                        tuples.Add(new Tuple<string, string>(id, newState));
                     }
                     else
                     {
-                        tuples = new List<Tuple<string, int>>();
-                        tuples.Add(new Tuple<string, int>(id, newState));
+                        tuples = new List<Tuple<string, string>>();
+                        tuples.Add(new Tuple<string, string>(id, newState));
                     }
-                    tree.Add(state, tuples);
+                    tree[state] = tuples;
                 }
                 graph.Load();
             }
