fc7384ee409de1d1ee03f953034472bf011f24b4
Robozou <14161475+Robozou@users.noreply.github.com>
2019-12-09 15:01:02 +0100

    Made the validator.

--- DCRReader/Validator.cs
+++ DCRReader/Validator.cs
@@ -1,14 +1,78 @@
 ﻿using bpmntrails;
+using System;
+using System.Collections.Generic;
 
 namespace DCRReader
 {
     //needs to visit each edge atleast once to see if traces expressed in bpmn is valid in dcr.
-    internal static class Validator
+    public class Validator
     {
-        static bool Validate(Processor graph, BPMNTrail trail)
+        Processor graph;
+        BPMNTrail trail;
+        Dictionary<string, string> labelId;
+        List<bool> results;
+        public Validator(Processor graph, BPMNTrail trail, Dictionary<string, string> labelId)
+        {
+            this.graph = graph;
+            this.trail = trail;
+            this.labelId = labelId;
+            results = new List<bool>();
+        }
+
+        public bool Validate()
+        {
+            StartEvent se = trail.Definition.process.startEvents[0];
+            RunStates(se, graph);
+            return results.TrueForAll(x => x.Equals(true));
+        }
+
+        private void RunStates(dynamic bpmnEvent, Processor graph)
         {
 
-            return false;
+            if (bpmnEvent.GetType().Equals(typeof(StartEvent)))
+            {
+                StartEvent se = (StartEvent)bpmnEvent;
+                foreach (string seqId in se.outgoing)
+                {
+                    RunStates(trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(seqId)), graph);
+                }
+            }
+            else if (bpmnEvent.GetType().Equals(typeof(EndEvent)))
+            {
+                results.Add(true);
+            }
+            else if (bpmnEvent.GetType().Equals(typeof(Task)))
+            {
+                Task task = (Task)bpmnEvent;
+                try
+                {
+                    graph.Execute(labelId[task.name]);
+                    foreach (string seqId in task.outgoing)
+                    {
+                        RunStates(trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(seqId)), graph);
+                    }
+                }
+                catch (Exception e)
+                {
+                    results.Add(false);
+                }
+            }
+            else if (bpmnEvent.GetType().Equals(typeof(ParallelGateway)))
+            {
+                ParallelGateway pg = (ParallelGateway)bpmnEvent;
+                foreach (string seqId in pg.outgoing)
+                {
+                    RunStates(trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(seqId)), graph);
+                }
+            }
+            else if (bpmnEvent.GetType().Equals(typeof(ExclusiveGateway)))
+            {
+                ExclusiveGateway eg = (ExclusiveGateway)bpmnEvent;
+                foreach (string seqId in eg.outgoing)
+                {
+                    RunStates(trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(seqId)), graph);
+                }
+            }
         }
     }
 }
