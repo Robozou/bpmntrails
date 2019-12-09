1315316867f9d2833d55c2bdff16066ac618ea51
Robozou <14161475+Robozou@users.noreply.github.com>
2019-12-09 13:59:38 +0100

    Added new validator class.

---
+++ DCRReader/Validator.cs
@@ -0,0 +1,14 @@
+﻿using bpmntrails;
+
+namespace DCRReader
+{
+    //needs to visit each edge atleast once to see if traces expressed in bpmn is valid in dcr.
+    internal static class Validator
+    {
+        static bool Validate(Processor graph, BPMNTrail trail)
+        {
+
+            return false;
+        }
+    }
+}
