15f644e42969048dea13e44338b66c795f9496ce
fujfuj <57488731+fujfuj@users.noreply.github.com>
2019-12-19 14:05:00 +0100

    Made method to remove single repeating events.

--- DCRReader/XMLOptimizer.cs
+++ DCRReader/XMLOptimizer.cs
@@ -11,39 +11,84 @@ namespace DCRReader
         int buffer = 0;
         internal BPMNTrail Optimize(Processor graph, List<List<string>> traces, Dictionary<string, HashSet<Tuple<string, string>>> tree, BPMNTrail trail, Dictionary<string, string> labelId)
         {
+            //MAKE A TEST CASE WHERE THIS IS TRUE PLZ
+            //FIND WAY TO LOOP EVEN ON A SINGLE EVENT IF IT OCCURS SEVERAL TIMES
+            //test if we can execute the events in the graph if we skip the events lying in between
+            //the the original list and the repeats i.e. if a1 -> a2 -> a3 -> a1 -> a2 -> a4 is the original
+            //then a1 -> a2 -> a4 must also be possible
+            //if this is the case remove the events in the bpmn that corrosponds to the second a1 -> a2
+            //add a merge gate infront of the first a1 -> a2 let a3 point to that and let the first a2 -> a4
+            //also find a way to handle when we have several alternating states fx a1 -> a2 -> a1 -> a2 -> ...
+            //test that all new executions that may result from the change is still valid in dcr
+            //collapse diverging gates if chained
+            //finally sweep and remove any merge/xor gates with a 1:1 in/out
+
+            //Maybe take longest found repeating sequence do the thing, and then rerun the finder etc.
+
             Cleaner cleaner = new Cleaner(traces, labelId);
+            Validator validator = new Validator(graph, labelId);
             BPMNTrail workingTrail;
-            RemoveRepeatingSequences(graph, traces, trail, labelId, out workingTrail);
+            RemoveRepeatingSequences(graph, traces, trail, labelId, validator, out workingTrail);
+            RemoveRepeatingEvents(graph, traces, workingTrail, labelId, validator, out workingTrail);
             workingTrail = cleaner.Clean(workingTrail);
             return workingTrail;
         }
 
-        private void RemoveRepeatingSequences(Processor graph, List<List<string>> traces, BPMNTrail trail, Dictionary<string, string> labelId, out BPMNTrail workingTrail)
+        private void RemoveRepeatingEvents(Processor graph, List<List<string>> traces, BPMNTrail trail, Dictionary<string, string> labelId, Validator validator, out BPMNTrail workingTrail)
+        {
+            workingTrail = trail;
+            BPMNTrail oldTrail;
+            List<string> ids = FindRepeatingEvents(traces);
+            foreach(string id in ids)
+            {
+                oldTrail = workingTrail;
+                workingTrail.RemoveTaskAndMoveSequences(id);
+                if (!validator.Validate(workingTrail))
+                {
+                    workingTrail = oldTrail;
+                }
+            }
+        }
+
+        private List<string> FindRepeatingEvents(List<List<string>> traces)
+        {
+            List<string> ids = new List<string>();
+            foreach(List<string> trace in traces)
+            {
+                for(int startLoc = 0; startLoc < trace.Count-1; startLoc++)
+                {
+                    for(int currLoc = startLoc+1; currLoc < trace.Count; currLoc++)
+                    {
+                        if (trace[startLoc].Equals(trace[currLoc]))
+                        {
+                            string id = string.Empty;
+                            for(int i = 0; i<=currLoc; i++)
+                            {
+                                id += trace[i];
+                            }
+                            ids.Add(id);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return ids;
+        }
+
+        private void RemoveRepeatingSequences(Processor graph, List<List<string>> traces, BPMNTrail trail, Dictionary<string, string> labelId, Validator validator, out BPMNTrail workingTrail)
         {
             Dictionary<List<string>, Tuple<int, List<int>>> dict;
             Dictionary<string, string> idGate = new Dictionary<string, string>();
-            Validator validator = new Validator(graph, labelId);
             workingTrail = trail;
             BPMNTrail oldTrail;
             foreach (List<string> l in traces)
             {
                 dict = FindRepeatingSequence(l);
                 if (dict.Keys.Count > 0)
-                {
-                    //MAKE A TEST CASE WHERE THIS IS TRUE PLZ
-                    //FIND WAY TO LOOP EVEN ON A SINGLE EVENT IF IT OCCURS SEVERAL TIMES
-                    //test if we can execute the events in the graph if we skip the events lying in between
-                    //the the original list and the repeats i.e. if a1 -> a2 -> a3 -> a1 -> a2 -> a4 is the original
-                    //then a1 -> a2 -> a4 must also be possible
-                    //if this is the case remove the events in the bpmn that corrosponds to the second a1 -> a2
-                    //add a merge gate infront of the first a1 -> a2 let a3 point to that and let the first a2 -> a4
-                    //also find a way to handle when we have several alternating states fx a1 -> a2 -> a1 -> a2 -> ...
-                    //test that all new executions that may result from the change is still valid in dcr
-                    //collapse diverging gates if chained
-                    //finally sweep and remove any merge/xor gates with a 1:1 in/out
-
-                    //Maybe take longest found repeating sequence do the thing, and then rerun the finder etc.
-
+                { 
                     if (dict.Keys.Count > 1)
                     {
                         List<List<string>> subs = new List<List<string>>();
