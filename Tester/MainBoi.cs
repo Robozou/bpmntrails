f94d50cd860f72a07607a41dff73d9630aee9408
fujfuj <57488731+fujfuj@users.noreply.github.com>
2019-12-19 14:47:58 +0100

    Made a main method to be used in the cmd.

---
+++ Tester/MainBoi.cs
@@ -0,0 +1,31 @@
+﻿using DCRReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    class MainBoi
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("--Must provide location of output, dcr graph and trace list, in that order.\n--And lastly add -f if you want the unoptimized result.");
+                return;
+            }
+            string fileLoc = args[0];
+            string dcrGraphLoc = args[1];
+            string traceListLoc = args[2];
+            bool optimize = true;
+            if(args.Length >= 4 && args[3].Equals("-f"))
+            {
+                optimize = false;
+            }
+            XMLReader xml = new XMLReader();
+            xml.Read(fileLoc, dcrGraphLoc, traceListLoc, optimize);
+        }
+    }
+}
