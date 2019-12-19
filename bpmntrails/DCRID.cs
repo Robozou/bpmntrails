68f85e1a4e8f50dec60a6e545f669ff5bb08d4ac
fujfuj <57488731+fujfuj@users.noreply.github.com>
2019-12-19 09:56:04 +0100

    Added a new extension to the bpmn xml.

---
+++ bpmntrails/DCRID.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace bpmntrails
+{
+    [XmlRoot(ElementName = "DCRID")]
+    public class DCRID
+    {
+        [XmlAttribute(AttributeName = "dcrid")]
+        public string dcrid { get; set; }
+    }
+}
--- bpmntrails/Task.cs
+++ bpmntrails/Task.cs
@@ -10,6 +10,8 @@ namespace bpmntrails
         public List<string> incoming = new List<string>();
         [XmlElement(ElementName = "outgoing", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
         public List<string> outgoing = new List<string>();
+        [XmlElement(ElementName = "extensionElements", Namespace = "http://www.omg.org/spec/BPMN/20100524/MODEL")]
+        public List<DCRID> extensionElements = new List<DCRID>();
         [XmlAttribute(AttributeName = "id")]
         public string id { get; set; }
         [XmlAttribute(AttributeName = "name")]
