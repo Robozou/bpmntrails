47e601c4172776d6588ed0366ecf9177ec9fd10f
Robozou <14161475+Robozou@users.noreply.github.com>
2020-01-10 11:21:42 +0100

    Stable point where it only allows for to many end events.

--- DCRReader/Cleaner.cs
+++ DCRReader/Cleaner.cs
@@ -1,5 +1,4 @@
 ﻿using bpmntrails;
-using System;
 using System.Collections.Generic;
 
 namespace DCRReader
@@ -7,15 +6,21 @@ namespace DCRReader
     internal class Cleaner
     {
         private List<List<string>> traces;
-        private Dictionary<string, string> labelId;
         private BPMNTrail trail;
-        HashSet<string> bpmnElements;
+        private Dictionary<string, string> labelId;
+        private Dictionary<List<string>, int> traceStatus;
+        private HashSet<string> bpmnElements;
 
         public Cleaner(List<List<string>> traces, Dictionary<string, string> labelId)
         {
             this.traces = traces;
             this.labelId = labelId;
+            traceStatus = new Dictionary<List<string>, int>();
             bpmnElements = new HashSet<string>();
+            foreach (List<string> trace in traces)
+            {
+                traceStatus[trace] = 0;
+            }
         }
 
         public BPMNTrail Clean(BPMNTrail trail)
@@ -23,7 +28,6 @@ namespace DCRReader
             this.trail = trail;
             FindEvents();
             RemoveEvents(); //This should be expanded to also find end nodes.
-            //RemoveDanglingEndEvents(); This should be removed.
             RemoveRedundantMergeAndSplitNodes(); //Maybe expand this to include the ability to join linked gates.
             return trail;
         }
@@ -45,73 +49,53 @@ namespace DCRReader
             toBeDel.ForEach(x => trail.RemoveTaskAndMoveSequences(x));
         }
 
-        private void RemoveDanglingEndEvents()
-        {
-            List<string> toBeDel = new List<string>();
-            foreach (EndEvent ee in trail.Definition.process.endEvents)
-            {
-                foreach (string id in ee.incoming)
-                {
-                    String sourceId = trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(id)).sourceRef;
-                    if (trail.Definition.process.startEvents.Exists(x => x.id.Equals(sourceId)))
-                    {
-                        toBeDel.Add(ee.id);
-                    }
-                    else if (trail.Definition.process.parallelGateways.Exists(x => x.id.Equals(sourceId)))
-                    {
-                        toBeDel.Add(ee.id);
-                    }
-                    else if (trail.Definition.process.exclusiveGateways.Exists(x => x.id.Equals(sourceId)))
-                    {
-                        toBeDel.Add(ee.id);
-                    }
-                }
-            }
-            toBeDel.ForEach(x => trail.RemoveEventWithSequences(x));
-        }
-
         private void FindEvents()
         {
             StartEvent se = trail.Definition.process.startEvents[0];
             bpmnElements.Add(se.id);
             foreach (List<string> trace in traces)
             {
-                trail.Definition.process.startEvents.Find(x => se.id.Equals(x.id)).outgoing.ForEach(x => FindEvent(x, trace, 0));
+                trail.Definition.process.startEvents.Find(x => se.id.Equals(x.id)).outgoing.ForEach(x => FindEvent(x, trace));
             }
         }
+
         // Skal gennemgå hele trace fra ende til anden ikke gennemgå grafen
-        private void FindEvent(string bpmnSeqId, List<string> trace, int index)
+        private void FindEvent(string bpmnSeqId, List<string> trace)
         {
             string targetId = trail.Definition.process.sequenceFlows.Find(x => x.id.Equals(bpmnSeqId)).targetRef;
-            if (trail.Definition.process.endEvents.Exists(x => x.id.Equals(targetId)))
+            if (trail.Definition.process.endEvents.Exists(x => x.id.Equals(targetId)) && traceStatus[trace] == trace.Count)
             {
+                bpmnElements.Add(targetId);
                 return;
             }
             else if (trail.Definition.process.exclusiveGateways.Exists(x => x.id.Equals(targetId)))
             {
-                ExclusiveGateway eg = trail.Definition.process.exclusiveGateways.Find(x => x.id.Equals(targetId));
-                foreach (string id in eg.outgoing)
-                {
-                    FindEvent(id, trace, index);
-                }
+                bpmnElements.Add(targetId);
+                trail.Definition.process.exclusiveGateways.Find(x => x.id.Equals(targetId)).outgoing.ForEach(x => FindEvent(x, trace));
             }
             else if (trail.Definition.process.parallelGateways.Exists(x => x.id.Equals(targetId)))
             {
-                ParallelGateway pg = trail.Definition.process.parallelGateways.Find(x => x.id.Equals(targetId));
-                foreach (string id in pg.outgoing)
-                {
-                    FindEvent(id, trace, index);
-                }
+                bpmnElements.Add(targetId);
+                trail.Definition.process.parallelGateways.Find(x => x.id.Equals(targetId)).outgoing.ForEach(x => FindEvent(x, trace));
             }
             else if (trail.Definition.process.tasks.Exists(x => x.id.Equals(targetId)))
             {
-                Task task = trail.Definition.process.tasks.Find(x => x.id.Equals(targetId));
-                if (trace.Count > index && labelId[task.name].Equals(trace[index]))
+                if (trace.Count > traceStatus[trace])
                 {
-                    bpmnElements.Add(task.id);
-                    foreach (string id in task.outgoing)
+                    Task task = trail.Definition.process.tasks.Find(x => x.id.Equals(targetId));
+                    string curr = string.Empty;
+                    for (int i = 0; i <= traceStatus[trace]; i++)
                     {
-                        FindEvent(id, trace, index + 1);
+                        curr += trace[i];
+                    }
+                    if (labelId[task.name].Equals(trace[traceStatus[trace]]))
+                    {
+                        bpmnElements.Add(task.id);
+                        traceStatus[trace]++;
+                        foreach (string id in task.outgoing)
+                        {
+                            FindEvent(id, trace);
+                        }
                     }
                 }
             }
